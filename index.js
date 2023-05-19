1df939c9cf7a64729745db5bb3166819ebab98d6
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2023-05-19 10:09:34 +0100

    code refactor

---
+++ index.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hambuger");
+const ul = document.querySelector(".open");
+
+hamburger.addEventListener("click", () => {
+    ul.classList.toggle("shower")
+})
