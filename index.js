e47c14fc660efc9e241e8948e30c4a08845f3640
Abdulsalamdev <abdulsalamakinyoola@gmail.com>
2023-12-10 07:25:56 +0100

    updating

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+// control for hambuger menu
 const hamburger = document.querySelector(".hambuger");
 const ul = document.querySelector(".open");
 
 hamburger.addEventListener("click", () => {
-    ul.classList.toggle("shower")
-})
+  ul.classList.toggle("shower");
+});
